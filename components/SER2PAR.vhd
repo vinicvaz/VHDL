35b3bc96118756485a06df0f7ce45914cf55f096
vinicvaz <vinicvaz95@gmail.com>
2019-11-26 16:33:34 -0300

    Add Ser-Par Conversor

--- components/SER2PAR.vhd
+++ components/SER2PAR.vhd
@@ -5,31 +5,44 @@ use ieee.std_logic_unsigned.all;
 
 entity SER2PAR is
 	Generic (
-		DATA_SIZE : integer := 8
+		DATA_SIZE : integer := 4
 	);
 	Port (
-		i_clk   : in  std_logic; -- CLOCK
-		i_ND    : in  std_logic; -- BORDA DETECTADA
-		i_DATA  : in  std_logic; -- DADO DE ENTRADA SERIAL
-		o_data  : out std_logic_vector ((DATA_SIZE-1) downto 0) -- dado de saida paralelo
+		i_clk      : in  std_logic; -- sinal de clock
+		i_rst      : in  std_logic; -- sinal de reset
+		i_ND       : in  std_logic; -- NEW DATA - converte quando ND = 1
+		i_DATA  	  : in  std_logic; -- dados de entrada (serial)
+		o_DATA	  : out std_logic_vector((DATA_SIZE-1) downto 0); -- dados de saida (paralelo) (buffer)
+		i_FREE	  : in std_logic
+
 	);
 end SER2PAR;
 
 architecture behavioral of SER2PAR is
 
-	signal w_DATA 	 : std_logic_vector (o_data'range);
+	signal w_DATA 	 : std_logic_vector ((DATA_SIZE-1) downto 0);
+	signal w_FREE	 : std_logic;
 	
 begin
 
-	process(i_clk) 
-	begin 
-		if falling_edge (i_clk) then 
-			if (i_ND = '1') then 
-				w_DATA <= w_DATA((DATA_SIZE-2) downto 0) & i_DATA;
-			end if; 
-      end if; 
-	end process;
-	
-	o_data <=w_DATA;
-	
+
+process (i_RST, i_CLK, i_FREE)
+begin
+
+	if (i_RST = '0') then
+		w_DATA <= (others=>'0');
+	elsif RISING_EDGE(i_CLK) then
+		if(i_ND = '1') then
+			w_DATA <= w_DATA((DATA_SIZE-2) downto 0) & i_DATA;
+		end if;
+	end if;
+end process;
+
+process(i_FREE)
+begin
+	if (i_FREE = '1') then -- w_FREE recebe 1 quando contador de bordas = 8, sinal chegou completo entao pode enviar para saida
+		o_DATA<= w_DATA;
+	end if;
+end process;
+
 end behavioral;
--- tests_benchs/TB_SER2PAR.vhd
+++ tests_benchs/TB_SER2PAR.vhd
@@ -1,128 +1,107 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_arith.ALL;
-use IEEE.STD_LOGIC_unsigned.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity TB_SER2PAR is
-	 Generic(
-	 		DATA_SIZE   : INTEGER := 8
-	 );
+	Generic (
+		DATA_SIZE : integer := 4
+	);
 end TB_SER2PAR;
 
 architecture Behavioral of TB_SER2PAR is
-	component SER2PAR is
-		Generic (
-			DATA_SIZE : integer := 8
-		);
-		Port (
-			i_clk   : in  std_logic;
-			i_ND    : in  std_logic;
-			i_DATA  : in  std_logic;
-			o_data  : out std_logic_vector ((DATA_SIZE-1) downto 0)
-		);
-	end component;
-	
-	SIGNAL w_CLK	: STD_LOGIC;
-	SIGNAL w_ND 	: STD_LOGIC;
-	SIGNAL w_DATA  : STD_LOGIC;
-	SIGNAL w_ODATA  : STD_LOGIC_VECTOR ((DATA_SIZE-1) downto 0);
-	signal w_SCLK	: std_logic;
-	
+
+---------------------------------------
+Component SER2PAR is
+	Port (
+		i_clk      : in  std_logic; -- sinal de clock
+		i_rst      : in  std_logic; -- sinal de reset
+		i_ND       : in  std_logic; -- NEW DATA - converte quando ND = 1
+		i_DATA  	  : in  std_logic; -- dados de entrada (serial)
+		o_DATA	  : out std_logic_vector((DATA_SIZE-1) downto 0); -- dados de saida (paralelo) (buffer)
+		i_FREE	  : in std_logic
+	);
+end component SER2PAR;
+--------------------------------------
+--------------------------------------
+SIGNAL w_CLK	: std_logic;
+SIGNAL w_RST	: std_logic;
+SIGNAL w_ND		: std_logic;
+SIGNAL w_DATA	: std_logic;
+SIGNAL w_ODATA	: std_logic_vector((DATA_SIZE-1) downto 0);
+SIGNAL w_FREE	: std_logic;
+SIGNAL w_SCLK  : std_logic; -- controle do sclk
+---------------------------------------
+---------------------------------------
+
 begin
 
-	UUT: SER2PAR 
-	Generic Map(
-	 	DATA_SIZE    => DATA_SIZE         
-	)
-   Port Map( 
-		i_clk 	=> w_CLK,
-		i_ND 		=> w_ND,
-		i_DATA  	=> w_DATA,
-		o_data  	=> w_ODATA
-	 );
+UUT	:	 SER2PAR
+	Port Map (
+		i_clk      => w_CLK,
+		i_rst      => w_RST,
+		i_ND       => w_ND,
+		i_DATA  	  => w_DATA,
+		o_DATA	  => w_ODATA,
+		i_FREE	  => w_FREE
+	);
 
-	-- processo SCLK pra controle (T = 1 us)
-	process
-	begin
-		w_SCLK<='1';
-		wait for 0.5 us;
-		w_SCLK<='0';
-		wait for 0.5 us;
+
+-- RESET PROCESS
+	process 
+		begin
+			w_RST <= '0';
+			wait for 10 us;
+			w_RST<= '1';
+			wait;
 	end process;
+
+-- CLOCK PROCESS
+	 process
+		 begin
+			 w_CLK <= '1';
+			 wait for 10 ns;
+			 w_CLK <= '0';
+			 wait for 10 ns;
+	 end process;
 	 
-	 
-	-- PROCESSO DO CLOCK
-	PROCESS
-	BEGin	
-		w_CLK <= '1';
-		WAIT FOR 10 ns;
-		w_CLK <= '0';
-		WAIT FOR 10 ns;
-	END PROCESS;
+-- SCLK PROCESS (PRA CONTROLE)
+	process
+		begin
+			w_SCLK <= '0';
+			wait for 1 us;
+			w_SCLK <= '1';
+			wait for 1 us;
+	end process;
 
+-- DATA PROCESS
+	process
+		begin
+			w_DATA<='0';
+			wait for 11 us;
+			w_DATA<='1';
+			wait for 2 us;
+			w_DATA<='0';
+			wait for 2 us;
+			w_DATA <='1';
+			wait for 2 us;
+			w_DATA<='1';
+			wait for 2 us;
+			w_FREE<='1';
+			wait for 20 ns;
+			w_FREE<='0';
+			wait;
+	end process;
 	
-	-- PROCESSO DO SINAL (DADOS COM PERIODO DE 1 CICLO DE SCLK)
-	--(COMO FAZER COM QUE O SINAL SEJA LENTO E O CLOCK RAPIDO E PASSE O DADO CERTO USANDO ND)
-
-	PROCESS
-	BEGin
+	-- SCLK SIGNAL PULSE ND (1MHz, 0.5 MHz cada borda)
+	process
+		begin
+		w_nd <= '0';
+		wait for 20 ns;
+		w_nd <= '1';
+		wait for 20 ns;
+		w_nd <= '0';
+		wait for 2 us; 
+	end process;
 	
-		
-		w_DATA    <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		WAIT FOR 1 us;
-		
-		
-		w_DATA   <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		
-		WAIT FOR 1 us;
-
-		w_DATA   <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		WAIT FOR 1 uS;
-		
-		w_DATA   <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		
-		WAIT FOR 1 uS;
-		
-		w_DATA   <= '1';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		WAIT FOR 1 uS;
-		
-		w_DATA   <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		
-		WAIT FOR 1 uS;
-		
-		w_DATA   <= '1';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-		WAIT FOR 1 us;
-
-		w_DATA   <= '0';
-		w_ND <= '1';
-		wait for 20 ns;
-		w_ND <='0';
-	
-		WAIT;
-	END PROCESS;
-	
-	-- ENABLE PROCESS
-	
-end Behavioral;
- 
+end behavioral;
